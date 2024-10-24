b961306f715559ea5e7b106881f6d782b472dc7f
T3tr1s <tomai.kornelmunka@gmail.com>
2024-10-25 00:37:57 +0200

    Add file searching and checking for camelCase within those files, then reporting it

--- replace_camelcase.py
+++ replace_camelcase.py
@@ -9,8 +9,35 @@ def validate_extensions(extensions):
     return True
 
 
+def get_source_files(source_path, extensions):
+    file_paths = []
+    for root, dirs, files in os.walk(source_path):
+        for file in files:
+            if file.endswith(tuple(extensions)):
+                file_paths.append(os.path.join(root, file))
+    return file_paths
+
+
+def check_files_for_camelcase(file_paths):
+    matches = []
+    line_number = 0
+    for file in file_paths:
+        with open(file) as f:
+            for line in f:
+                line_number += 1
+                match = re.search("\\W[a-zA-Z]+([A-Z][a-z]*)+\\W", line)
+                if match:
+                    matches.append(tuple([file, line_number, match.group()[1:-1], line]))
+    return matches
+
+
 def main(source, extensions):
-    print(f"Source: {source}\nExtensions: {extensions}")
+    source_path = os.path.join(os.getcwd(), source)
+    file_paths = get_source_files(source_path, extensions)
+    matches = check_files_for_camelcase(file_paths)
+
+    for match in matches:
+        print(match)
 
 
 if __name__ == "__main__":

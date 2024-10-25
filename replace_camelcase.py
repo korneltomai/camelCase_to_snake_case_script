be5125970e59f06a04e235ea9a6913efa5f47839
T3tr1s <tomai.kornelmunka@gmail.com>
2024-10-25 18:02:05 +0200

    Add some formatting for displaying matches and the option to select matches by the ranges of their indexes

--- replace_camelcase.py
+++ replace_camelcase.py
@@ -18,29 +18,42 @@ def get_source_files(source_path, extensions):
     return file_paths
 
 
-def check_files_for_camelcase(file_paths):
+def get_matches_from_file(source, file_paths):
     matches = []
     line_number = 0
     for file in file_paths:
         with open(file) as f:
             for line in f:
                 line_number += 1
-                match = re.search("\\W[a-zA-Z]+([A-Z][a-z]*)+\\W", line)
-                if match:
-                    matches.append(tuple([file, line_number, match.group()[1:-1], line]))
+                matches_in_line = re.findall("\\W[a-zA-Z]+(?:[A-Z][a-z]*)+\\W", line)
+                if matches_in_line:
+                    for match in matches_in_line:
+                        matches.append(tuple([os.path.relpath(file, source), line_number, match[1:-1], line]))
     return matches
 
 
-def main(source, extensions):
-    source_path = os.path.join(os.getcwd(), source)
-    file_paths = get_source_files(source_path, extensions)
-    matches = check_files_for_camelcase(file_paths)
-
+def print_matches(matches):
+    index = 1
+    print("―" * os.get_terminal_size().columns)
     for match in matches:
-        print(match)
+        print(f"{index}. {match}")
+        index += 1
+    print("―" * os.get_terminal_size().columns)
 
 
-if __name__ == "__main__":
+def get_selected_matches(matches):
+    match_indexes = []
+    input_ranges = input("Select matches to replace by providing at least one range in a 'index:index' format, separated by a comma (,): ")
+    while not matches:
+        input_ranges = input("Please enter at least one range: ")
+
+    matches = re.findall("[0-9]+:[0-9]+", input_ranges)
+    for match in matches:
+        match_indexes.append(tuple(match.split(":")))
+    return match_indexes
+
+
+def main():
     args = sys.argv[1:]
     if len(args) < 2:
         raise Exception("Invalid arguments. Please provide a valid path to a source directory and at least one file extension to scan.")
@@ -53,4 +66,14 @@ if __name__ == "__main__":
     if not validate_extensions(extensions):
         raise Exception("Invalid extension(s).")
 
-    main(source, extensions)
+    source_path = os.path.join(os.getcwd(), source)
+    file_paths = get_source_files(source_path, extensions)
+    matches = get_matches_from_file(source_path, file_paths)
+
+    print_matches(matches)
+    selected_matches = get_selected_matches(matches)
+    print(selected_matches)
+
+
+if __name__ == "__main__":
+    main()
